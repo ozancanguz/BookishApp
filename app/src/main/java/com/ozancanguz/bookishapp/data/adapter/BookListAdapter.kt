c62af128e6d87459f59d254c456289b60723e704
Ozan <ozancangu24@gmail.com>
2023-04-21 20:43:46 +0300

    -BookListAdapter created

---
+++ app/src/main/java/com/ozancanguz/bookishapp/data/adapter/BookListAdapter.kt
@@ -0,0 +1,46 @@
+package com.ozancanguz.bookishapp.data.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ozancanguz.bookishapp.R
+import com.ozancanguz.bookishapp.data.book.Book
+import com.ozancanguz.bookishapp.data.book.BookList
+import com.ozancanguz.bookishapp.databinding.BookRowLayoutBinding
+import com.ozancanguz.bookishapp.util.Util.Companion.loadImage
+
+class BookListAdapter : RecyclerView.Adapter<BookListAdapter.BookViewHolder>() {
+
+    inner class BookViewHolder(val binding: BookRowLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+    }
+
+    private var bookList = emptyList<BookList>()
+
+    fun setData(newData: Book) {
+        bookList = newData.result
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = BookRowLayoutBinding.inflate(inflater, parent, false)
+        return BookViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return bookList.size
+    }
+
+    override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
+        val currentBook = bookList[position]
+        holder.binding.bookTitle.text = currentBook.title
+        holder.binding.bookAuthor.text = currentBook.yazar
+        holder.binding.bookPrice.text = currentBook.fiyat
+        holder.binding.bookDiscount.text = currentBook.indirim
+        holder.binding.bookPublisher.text = currentBook.yayın
+        holder.binding.bookImage.loadImage(currentBook.image)
+
+
+    }
+}
--- app/src/main/java/com/ozancanguz/bookishapp/data/api/BookApi.kt
+++ app/src/main/java/com/ozancanguz/bookishapp/data/api/BookApi.kt
@@ -3,9 +3,14 @@ package com.ozancanguz.bookishapp.data.api
 import com.ozancanguz.bookishapp.data.book.Book
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface BookApi {
 
+    @Headers(
+        "Authorization: apikey 4IeDD4a3d0pzZpPENijBY4:3UABECFIqhaQo9m3uZT64A",
+        "Content-Type: application/json"
+    )
     @GET("newBook")
     suspend fun getBooks(): Response<Book>
 }
