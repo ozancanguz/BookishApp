509856c0385a32b5f8492b441b766200f983e0fa
Ozan <ozancangu24@gmail.com>
2023-04-21 20:05:58 +0300

    -Book.kt data class created from api.

---
+++ app/src/main/java/com/ozancanguz/bookishapp/data/book/Book.kt
@@ -0,0 +1,6 @@
+package com.ozancanguz.bookishapp.data.book
+
+data class Book(
+    val result: List<BookList>,
+    val success: Boolean
+)
---
+++ app/src/main/java/com/ozancanguz/bookishapp/data/book/BookList.kt
@@ -0,0 +1,11 @@
+package com.ozancanguz.bookishapp.data.book
+
+data class BookList(
+    val fiyat: String,
+    val image: String,
+    val indirim: String,
+    val title: String,
+    val url: String,
+    val yayın: String,
+    val yazar: String
+)
